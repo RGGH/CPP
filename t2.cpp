c173c728b3d246f120eb611caf6809f02a37f4ac
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-04-01 22:07:09 +0100

    Update t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -13,9 +13,8 @@ int main (void)
     
     *ptr_to_a = 6;  
     printf ("The value of a is %d\n", a);  
-    
-  printf ("The value of ptr_to_a is %f", ptr_to_a);  
+    printf ("The value of ptr_to_a is %f", ptr_to_a);  
     printf ("It stores the value %f\n", *ptr_to_a);  
-   printf ("The address of a is %f\n", &a);
+    printf ("The address of a is %f\n", &a);
     
 }
