00d3bc61351e16ee9855586e91461b014c190684
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-04-07 11:20:17 +0100

    Create arrptr.c

---
+++ arrptr.c
@@ -0,0 +1,32 @@
+// array of pointers 
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+    int nums[5] = {1,2,3,4,5};
+
+    int *ptr0 = &nums[0];
+    int *ptr1 = &nums[1];
+    int *ptr2 = &nums[2];
+    int *ptr3 = &nums[3];
+    int *ptr4 = &nums[4];
+
+    int *ptrs[5]= { ptr0,ptr1,ptr2,ptr3,ptr4 };
+
+    char str[9] = {'C',' ','i','s',' ','F','u','n','\0'};
+    char *string = str;
+    char *strings[3] = {"Alpha","Bravo","Charlie"};
+
+    for (i=0; i<5; i++)
+        {
+            printf( "the value at %p is: %d\n", ptrs[i], *ptrs[i] );
+        }
+    printf("\nString is: %s\n\n ",string);
+
+    for (i=0;i<3;i++)
+    {
+        printf("Strings value is %s, pointer is %p\n", strings[i],*strings[i] );
+    }
+
+}
