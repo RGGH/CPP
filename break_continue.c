e9fb4884bf3694272eabc81fb2e9a370d217ea5b
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-04-19 17:41:45 +0100

    Add files via upload

---
+++ break_continue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+
+    a=5;
+
+
+    while(a<13)
+    {
+        a+=1;
+        if(a==9){
+        break;
+        }
+        printf("%d ",a);
+    }
+    return 0;
+}
