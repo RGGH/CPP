9f82a49d8ea9752b24e43f87cbc73eb422c5dc54
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-09-06 12:41:55 +0100

    Update vp.c

--- vidiprinter/vp.c
+++ vidiprinter/vp.c
@@ -4,8 +4,24 @@
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 */
 
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+//There is no built-in "sleep" in C99, hence this function. You can use any method you want to implement a delay.
+void customSleep( int seconds )
+{   // Pretty cross-platform, both ALL POSIX compliant systems AND Windows
+    #ifdef _WIN32
+        Sleep( 1000 * seconds );
+    #else
+        sleep( seconds );
+    #endif
+}
+
 #include <stdio.h>
-#include <unistd.h>
+#include <time.h>
 
 /* prints source file (text) to new file, as if it is being typed by a human
 open up the dstfile and see it being typed during the execution of the program!
@@ -35,7 +51,7 @@ int main(int argc, char *argv[])
     /* ------------ Open source code file in read-only mode ---------*/
 
     fps = fopen(filename, "r");
-    fread(buffer, 1, 2000, fps);
+    fread(buffer, 1, 200, fps);
 
     /*------------- Open destination file in write mode -----------------*/
 
@@ -46,7 +62,7 @@ int main(int argc, char *argv[])
         printf("%s output file created\n", opfilename);
         for (i = 0; buffer[i]; i++)
         {
-            usleep(600000);
+            customSleep(1);
             fputc(buffer[i], fpd); // put one char
             fflush(fpd);           // flush one char
         }
